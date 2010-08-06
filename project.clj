7640f97e4613e8f2a722b56d9fa09d89df64b2b0
Jeff Palmucci <jpalmucci@csail.mit.edu>
2010-08-06 13:33:07 -0400

    Change project.clj to suit clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-yield "1.0.0-SNAPSHOT"
+(defproject clj-yield/clj-yield "1.0"
   :description "A function like python's yield statement that can push items to a lazy sequence from arbitrary (non-lazy) code."
   :dependencies [[org.clojure/clojure "1.2.0-RC1"] 
                  [org.clojure/clojure-contrib "1.2.0-RC1"]] )
