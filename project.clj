97f49e01be0bffbfcdeec5c44b3cd6a3ebae61f5
Jeff Palmucci <jpalmucci@csail.mit.edu>
2010-08-30 17:35:44 -0400

    move to release version of clojure

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-yield/clj-yield "1.0"
   :description "A function like python's yield statement that can push items to a lazy sequence from arbitrary (non-lazy) code."
-  :dependencies [[org.clojure/clojure "1.2.0-RC1"] 
-                 [org.clojure/clojure-contrib "1.2.0-RC1"]
-                 [clj-iterate/clj-iterate "0.9"]] )
+  :dependencies [[org.clojure/clojure "1.2.0"] 
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [clj-iterate/clj-iterate "0.91"]] )
 
