a5fd514ffbfccf401e535c4ec01bac5e518811c7
Jeff Palmucci <jpalmucci@csail.mit.edu>
2012-09-21 09:56:44 -0400

    cut a new version and deploy

--- project.clj
+++ project.clj
@@ -1,6 +1,2 @@
-(defproject clj-yield/clj-yield "1.1-SNAPSHOT"
-  :description "A function like python's yield statement that can push items to a lazy sequence from arbitrary (non-lazy) code."
-    :dev-dependencies [[lein-multi "1.1.0-SNAPSHOT"]]
-    :multi-deps {"1.3" [[org.clojure/clojure "1.3.0-beta3"]]
-                 "1.2" [[org.clojure/clojure "1.2.1"]]} )
-
+(defproject clj-yield/clj-yield "1.1"
+  :description "A function like python's yield statement that can push items to a lazy sequence from arbitrary (non-lazy) code." )
